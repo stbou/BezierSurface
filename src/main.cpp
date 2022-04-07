7a0045916bffe1000eb22c440fbed5e4cd9a5424
Stéphane Boulanger <Stphane Boulanger@LAPTOP-R5MCKF6L>
2022-04-06 23:52:09 -0400

    Ajoutez des fichiers projet.

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * \file main.cpp
+ * \author Stephane Boulanger
+ * \version 0.1
+ * \date 2022-04-06
+ * \copyright Copyright (c) 2022
+ */
+
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
+}
